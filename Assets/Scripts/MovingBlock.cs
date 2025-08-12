0423230f009080978a30a58d14a6ca83602fe854
Kazin7 <krustm8@naver.com>
2025-08-12 21:10:06 +0900

    Feat : 움직이는 블록 및 플레이어도 같이이동하는 로직구현

---
+++ Assets/Scripts/MovingBlock.cs
@@ -0,0 +1,71 @@
+using UnityEngine;
+using System.Collections.Generic;
+
+public class MovingBlock : MonoBehaviour
+{
+    [Header("Move Settings")]
+    [SerializeField] float speed = 3f;
+    [SerializeField] float minZ = -10f;
+    [SerializeField] float maxZ = 6f;
+
+    [Header("Carrier (Empty)")]
+    [SerializeField] Transform carrierRoot;
+
+    Rigidbody blockRb;
+    Vector3 startLocalPosOfBlock;
+    Vector3 lastPosition;
+
+    private HashSet<Transform> riders = new HashSet<Transform>();
+
+    void Awake()
+    {
+        if (carrierRoot == null)
+            carrierRoot = transform.parent != null ? transform.parent : transform;
+
+        blockRb = carrierRoot.GetComponent<Rigidbody>();
+        if (blockRb == null)
+            blockRb = carrierRoot.gameObject.AddComponent<Rigidbody>();
+
+        blockRb.isKinematic = true;
+        blockRb.interpolation = RigidbodyInterpolation.Interpolate;
+    }
+
+    void Start()
+    {
+        startLocalPosOfBlock = carrierRoot.localPosition;
+        lastPosition = carrierRoot.position;
+    }
+
+    void FixedUpdate()
+    {
+        float zRange = Mathf.Abs(maxZ - minZ);
+        float z = Mathf.PingPong(Time.time * speed, zRange) + Mathf.Min(minZ, maxZ);
+
+        Vector3 lp = startLocalPosOfBlock;
+        lp.z = z;
+
+        Vector3 worldPos = carrierRoot.parent ? carrierRoot.parent.TransformPoint(lp) : lp;
+
+        Vector3 deltaMove = worldPos - lastPosition;
+
+        blockRb.MovePosition(worldPos);
+
+        foreach (var rider in riders)
+        {
+            rider.position += deltaMove;
+        }
+
+        lastPosition = worldPos;
+    }
+
+
+    private void OnCollisionEnter(Collision collision)
+    {
+        riders.Add(collision.transform);
+    }
+
+    private void OnCollisionExit(Collision collision)
+    {
+        riders.Remove(collision.transform);
+    }
+}
